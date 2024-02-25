cccf9b558360f086eb63104b1c0c712431b802bf
Nicole Arwood <nsarwood@gmail.com>
2024-02-25 05:15:07 -0500

    Added npm files

--- sandboxIndex.js
+++ sandboxIndex.js
@@ -1,3 +1,15 @@
+export function getData() { 
+    fetch("./database.json")
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            console.log(response.body.getReader().read());
+        });
+
+    }
+
+export const dict = {"1919: The Year of the Crack-Up": {"x": 0.0, "y": 0.0}, "In Search of Lost Screen Time": {"x": 0.0, "y": 0.0}, "Warren Wells, Star Receiver With a Derailed Career, Dies at 76": {"x": -0.15, "y": 0.5}, "2019: The Year of the Wolves": {"x": 0.0, "y": 0.0}}
 
 
 export const xyvals = [
@@ -7,4 +19,6 @@ export const xyvals = [
     ];
 
 export const label_array = ["Label 1", "Label 2", "Label 3"];
+
+
 
